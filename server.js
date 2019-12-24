779362ef23cbc2051e8f1e15e8ed7bd9b4b2e0cd
Eugene Koen <eugenekoenn@gmail.com>
2019-12-25 00:50:37 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+var cors=require('cors');
+var path=require('path');
+
+var app=express();
+
+app.use(bodyParser());
+app.use(cors());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response){
+    response.render('index');
+});
+
+app.get('/about', function(request, response){
+    response.render('about');
+});
+
+app.get('/contact', function(request, response){
+    response.render('contact');
+});
+
+app.listen(3000, function(){
+    console.log("heard on 3000");
+});
